6e902d57eccba9846ea2410170d6894f08c4e4c2
Bruno Faria <brunocfnba@gmail.com>
2017-11-08 17:36:24 -0200

    Update sentiment_neural_net.py

--- sentiment_neural_net.py
+++ sentiment_neural_net.py
@@ -1,58 +1,61 @@
 import tensorflow as tf
 import pickle
-import numpy as np
 
 x = tf.placeholder('float')
 y = tf.placeholder('float')
 
-nodes_hl1 = 1500
-nodes_hl2 = 1500
-nodes_hl3 = 1500
-
-n_classes = 2
-batch_size = 100
-epochs = 10
-
-# opens the pickle file with hot vectors
-with open('sentiment_set.pickle', 'rb') as f:
-    train_x, train_y, test_x, test_y = pickle.load(f)
+batch_size = 1000
+num_epochs = 1
 
 
-# Create all the weights and biases TensorFlow variables for each layer
-layer1 = {'weight': tf.Variable(tf.random_normal([len(train_x[0]), nodes_hl1]), name='w1'),
-          'bias': tf.Variable(tf.random_normal([nodes_hl1]), name='b1')}
-
-layer2 = {'weight': tf.Variable(tf.random_normal([nodes_hl1, nodes_hl2]), name='w2'),
-          'bias': tf.Variable(tf.random_normal([nodes_hl2]), name='b2')}
-
-layer3 = {'weight': tf.Variable(tf.random_normal([nodes_hl2, nodes_hl3]), name='w3'),
-          'bias': tf.Variable(tf.random_normal([nodes_hl3]), name='b3')}
-
-output_layer = {'weight': tf.Variable(tf.random_normal([nodes_hl3, n_classes]), name='wo'),
-                'bias': tf.Variable(tf.random_normal([n_classes]))}
+def load_details():
+    with open('data_details.pkl', 'rb') as details:
+        det = pickle.load(details)
+        return det
 
 
-# Actually creates the neural network model
-def nn_model(input_data):
+line_sizes = load_details()
 
-    l1 = tf.add(tf.matmul(input_data, layer1['weight']), layer1['bias'])
+
+# Creates the neural network model
+def ff_neural_net(input_data):
+    neurons_hl1 = 1500
+    neurons_hl2 = 1500
+    neurons_hl3 = 1500
+
+    output_neurons = 2
+
+    l1_weight = tf.Variable(tf.random_normal([line_sizes['dict'], neurons_hl1]), name='w1')
+    l1_bias = tf.Variable(tf.random_normal([neurons_hl1]), name='b1')
+
+    l2_weight = tf.Variable(tf.random_normal([neurons_hl1, neurons_hl2]), name='w2')
+    l2_bias = tf.Variable(tf.random_normal([neurons_hl2]), name='b2')
+
+    l3_weight = tf.Variable(tf.random_normal([neurons_hl2, neurons_hl3]), name='w3')
+    l3_bias = tf.Variable(tf.random_normal([neurons_hl3]), name='b3')
+
+    output_weight = tf.Variable(tf.random_normal([neurons_hl3, output_neurons]), name='wo')
+    output_bias = tf.Variable(tf.random_normal([output_neurons]), name='bo')
+
+    l1 = tf.add(tf.matmul(input_data, l1_weight), l1_bias)
     l1 = tf.nn.relu(l1)
 
-    l2 = tf.add(tf.matmul(l1, layer2['weight']), layer2['bias'])
+    l2 = tf.add(tf.matmul(l1, l2_weight), l2_bias)
     l2 = tf.nn.relu(l2)
 
-    l3 = tf.add(tf.matmul(l2, layer3['weight']), layer3['bias'])
+    l3 = tf.add(tf.matmul(l2, l3_weight), l3_bias)
     l3 = tf.nn.relu(l3)
 
-    output = tf.matmul(l3, output_layer['weight']) + output_layer['bias']
+    output = tf.matmul(l3, output_weight) + output_bias
 
     return output
 
 
-def train_neural_network(x):
-    prediction = nn_model(x)
+def training(in_placeholder):
+    nn_output = ff_neural_net(in_placeholder)
+    saver = tf.train.Saver()
     # We are using cross entropy to calculate the cost
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=nn_output, labels=y))
 
     # and Gradient Descent to reduce the cost
     optimizer = tf.train.AdamOptimizer(learning_rate=0.001).minimize(cost)
@@ -60,26 +63,44 @@ def train_neural_network(x):
     # A TensorFLow session is created that will actually run the previously defined graph
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for epoch in range(epochs):
+        # saver = tf.train.Saver()
+        for epoch in range(num_epochs):
             epoch_loss = 0
-            i = 0
+            buffer_train = []
+            buffer_label = []
+            with open('train_hot_vectors.pickle', 'rb') as train_hot_vec:
+                for i in range(line_sizes['train']):
+                    hot_vector_line = pickle.load(train_hot_vec)
+                    buffer_train.append(hot_vector_line[0])
+                    buffer_label.append(hot_vector_line[1])
 
-            # this where we send chunks of data to be trained against our model
-            while i < len(train_x):
-                start = i
-                end = i+batch_size
-                chunk_x = np.array(train_x[start:end])
-                chunk_y = np.array(train_y[start:end])
+                    # print('Bla:' + str(buffer_label))
 
-                _, c = sess.run([optimizer, cost], feed_dict={x: chunk_x, y: chunk_y})
-                epoch_loss += c
-                i += batch_size
+                    if len(buffer_train) >= batch_size:
+                        _, cost_iter = sess.run([optimizer, cost],
+                                                feed_dict={in_placeholder: buffer_train, y: buffer_label})
+                        epoch_loss += cost_iter
+                        buffer_train = []
+                        buffer_label = []
 
-            print('Epoch', epoch+1, 'completed out of', epochs, 'loss:', epoch_loss)
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+            print('Epoch {} completed. Total loss: {}'.format(epoch+1, epoch_loss))
+
+        correct = tf.equal(tf.argmax(nn_output, 1), tf.argmax(y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
 
+        with open('test_hot_vectors.pickle', 'rb') as train_hot_vec:
+            buffer_test = []
+            buffer_test_label = []
+            for i in range(line_sizes['test']):
+                test_hot_vector_line = pickle.load(train_hot_vec)
+                buffer_test.append(test_hot_vector_line[0])
+                buffer_test_label.append(test_hot_vector_line[1])
+
         # the accuracy is the percentage of hits
-        print('Accuracy:', accuracy.eval({x: test_x, y: test_y}))
-        saver = tf.train.Saver()
+        print('Accuracy using test dataset: {}'
+              .format(accuracy.eval({in_placeholder: buffer_test, y: buffer_test_label})))
+        # saver = tf.train.Saver()
         saver.save(sess, "model.ckpt")
+
+
+# training(x)
